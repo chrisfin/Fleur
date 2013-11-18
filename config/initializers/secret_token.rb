22e2ceb755056547edd4e954960e83cd07e5b5c9
Chris Finlayson <chrisfinlayson@gmail.com>
2013-11-17 22:45:37 -0500

    First workable version of Fleur

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Fleur::Application.config.secret_key_base = '55489ed77853041609c2d7331089e9e6c60dabfbe4fc2750991d57b458e21c8ff5a388bcb606e65604d29fa24cc303e63399ce6752eb16ea1ac197305a8be8b9'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Fleur::Application.config.session_store :cookie_store, key: '_Fleur_session'
